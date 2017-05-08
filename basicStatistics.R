e0eb3612c1017b4d72e8a16c3170080dbae61f59
Markus Monz <markusmonz@googlemail.com>
2017-05-08 20:03:08 +0200

    add: first plot of decision distribution

--- basicStatistics.R
+++ basicStatistics.R
@@ -1,23 +1,32 @@
 basicStatistics <- function(groupData, opinionAfter) {
   groupCount <- length(which(!is.na(groupData[[opinionAfter]])))
-  deniedCount <- length(which(groupData[[opinionAfter]] == 3))
-  neutralCount <- length(which(groupData[[opinionAfter]] == 2))
-  agreeCount <- length(which(groupData[[opinionAfter]] == 1))
+  
+  deniedCountBefore <- length(which(groupData$opinion_before == 3 & !is.na(groupData[[opinionAfter]])))
+  neutralCountBefore <- length(which(groupData$opinion_before == 2 & !is.na(groupData[[opinionAfter]])))
+  agreeCountBefore <- length(which(groupData$opinion_before == 1 & !is.na(groupData[[opinionAfter]])))
+  
+  deniedCountAfter <- length(which(groupData[[opinionAfter]] == 3))
+  neutralCountAfter <- length(which(groupData[[opinionAfter]] == 2))
+  agreeCountAfter <- length(which(groupData[[opinionAfter]] == 1))
+  
   decisionRemainedCount <- length(which(groupData[[opinionAfter]] == groupData$opinion_before))
   decisionChangedCount <- length(which(groupData[[opinionAfter]] != groupData$opinion_before))
   decisionChangedPositiveCount <- length(which((groupData[[opinionAfter]] == 1 & groupData$opinion_before >= 2) | (groupData[[opinionAfter]] == 2 & groupData$opinion_before == 3) ))
   decisionChangedNegativeCount <- length(which((groupData[[opinionAfter]] == 3 & groupData$opinion_before <= 2) | (groupData[[opinionAfter]] == 2 & groupData$opinion_before == 1) ))
+  
   decisionChangedPercent <- decisionChangedCount / groupCount
   decisionChangedPositivePercent <- decisionChangedPositiveCount / decisionChangedCount
   decisionChangedNegativePercent <- decisionChangedNegativeCount / decisionChangedCount
 
-  df <- data.frame(groupCount, deniedCount, neutralCount, agreeCount,
+  df <- data.frame(groupCount, deniedCountAfter, neutralCountAfter, agreeCountAfter,
+                   deniedCountBefore, neutralCountBefore, agreeCountBefore,
                    decisionRemainedCount, decisionChangedCount,
                    decisionChangedPositiveCount, decisionChangedNegativeCount,
                    decisionChangedPercent, decisionChangedPositivePercent,
                    decisionChangedNegativePercent)
 
-  colnames(df) <- c("groupCount", "deniedCount", "neutralCount", "agreeCount",
+  colnames(df) <- c("groupCount", "deniedCount.After", "neutralCount.After", "agreeCount.After",
+                    "deniedCount.Before", "neutralCount.Before", "agreeCount.Before",
                    "decisionRemainedCount", "decisionChangedCount",
                    "decisionChangedPositiveCount", "decisionChangedNegativeCount",
                    "decisionChangedPercent", "decisionChangedPositivePercent",
--- surveyTest.R
+++ surveyTest.R
@@ -25,6 +25,11 @@ partialResponseCount <-length(which(isPartial))
 surveyDataCompleted <- surveyData[isComplete,]
 surveyDataPartial <- surveyData[isPartial,]
 
+# questionnaire resulsts
+questionnaireResults <- select(surveyDataCompleted, contains("personality_questionnaire"))
+questionnaireMeans <- data.frame(sapply(questionnaireResults, mean))
+questionnaireStandardDeviations <- data.frame(sapply(questionnaireResults, sd))
+
 # test group
 testStatistics <- basicStatistics(surveyDataCompleted, "opinion_after_test")
 testStatisticsByPersonality <- personalityTypesStatistics(surveyDataCompleted, personalityTypes)
@@ -34,3 +39,20 @@ controlStatistics <- basicStatistics(surveyDataCompleted, "opinion_after_control
 
 # combine
 combinedStatistics <- rbind(testStatistics, controlStatistics)
+combinedStatistics <- mutate(combinedStatistics, group = c("test", "control"))
+
+# decision distribution + plot
+decisionDistribution <- select(combinedStatistics, deniedCount.After:agreeCount.Before, group)
+colnames(decisionDistribution) <- gsub("Count", "", colnames(decisionDistribution), fixed = TRUE)
+decisionDistribution <- gather(decisionDistribution, decision, count, denied.After:agree.Before)
+decisionDistribution <- separate(decisionDistribution, decision, c("decision", "state"), sep = "\\.", remove = TRUE)
+decisionDistribution <- arrange(decisionDistribution, desc(group), desc(state))
+decisionDistribution <- decisionDistribution %>% group_by(group, state) %>% mutate(percentage=100 * (count/sum(count)))
+## pie plot
+par(mfrow=c(2,2))
+# test
+with(decisionDistribution, pie(percentage[group == "test" & state == "Before"], paste(decision, sprintf("%.2f", percentage[group == "test" & state == "Before"])), main = "test group before persuasion"))
+with(decisionDistribution, pie(percentage[group == "test" & state == "After"], paste(decision, sprintf("%.2f", percentage[group == "test" & state == "After"])), main = "test group after persuasion"))
+# control
+with(decisionDistribution, pie(percentage[group == "control" & state == "Before"], paste(decision, sprintf("%.2f", percentage[group == "control" & state == "Before"])), main = "control group before persuasion"))
+with(decisionDistribution, pie(percentage[group == "control" & state == "After"], paste(decision, sprintf("%.2f", percentage[group == "control" & state == "After"])), main = "control group after persuasion"))
