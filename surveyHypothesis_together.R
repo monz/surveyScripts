1bff592bc04ae72b0bc2c0fe183ae8ff0c2b1354
Markus Monz <markusmonz@googlemail.com>
2017-08-31 12:08:38 +0200

    update: hypotheses testing

--- surveyHypothesis_together.R
+++ surveyHypothesis_together.R
@@ -1,15 +1,11 @@
 # import external libraries
 library(gmodels)
-library(ggplot2)
 
 # import functions
 source("rworkspace/surveyTest/loadData.R")
 
-## set font size
-theme_set(theme_gray(base_size = 20))
-
 ## hypothesis testing
-#### neg+neut, pos
+#### opinion change clustered
 opinionChangeTable <- xtabs(~ group + opinion_changed_two_levels, data = surveyDataCombinedClean)
 
 testOpinionChangedByGroup <- fisher.test(opinionChangeTable, alternative = "greater")
@@ -21,17 +17,15 @@ opinionChangeNfcTable <- xtabs(~ group + opinion_changed_two_levels + nfcR, data
 # create saturated model
 saturated <- MASS::loglm(~ group*opinion_changed_two_levels*nfcR, data = opinionChangeNfcTable, fit = TRUE)
 # create next simpler model
-#s1 <- loglm(~ group + opinion_changed_two + nfcR + group:opinion_changed_two + group:nfcR + nfcR:opinion_changed_two, data = opinionChangeNfcTable)
 s1 <- update(saturated, .~. -group:opinion_changed_two_levels:nfcR)
 # create next simpler model
 s2 <- update(s1, .~. -nfcR:opinion_changed_two_levels)
-s3 <- update(s1, .~. -group:nfcR) # treatment:nfcR
-s4 <- update(s1, .~. -group:opinion_changed_two_levels) # treatment:opinion_changed_two
+s3 <- update(s2, .~. -group:opinion_changed_two_levels)
 # examine results
 print(anova(saturated, s1))
 print(anova(s1, s2))
-print(anova(s1, s3))
-print(anova(s1, s4))
-# plot table
+print(anova(s2, s3))
+
+## plot standardized residuals
 mosaicplot(opinionChangeNfcTable, shade = TRUE, xlab = "Group", ylab = "Opinion Change", main = "", cex.axis = 1)
 mosaicplot(opinionChangeTable, shade = TRUE, xlab = "Group", ylab = "Opinion Change", main = "", cex.axis = 1)
